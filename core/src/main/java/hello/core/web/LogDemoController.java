11d1338812f5918ddcda0fca2b3b7b500191f64f
zingwang <zingwang06@naver.com>
2023-03-18 10:41:50 +0900

    스코프+프록시

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-    //private final MyLogger myLogger;
+    //private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger= myLoggerProvider.getObject();
+        MyLogger myLogger= new MyLogger();
         String requestURL = request.getRequestURL().toString();
 
         myLogger.setRequestURL(requestURL);
